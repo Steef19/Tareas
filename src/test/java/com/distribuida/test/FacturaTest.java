4e6d3bfe4bef57f69c836484ff559921e83c4a96
1755804091 <bguaman@tecnologicoismac.edu.ec>
2025-05-28 15:20:11 -0500

    prueba3 test F

--- src/test/java/com/distribuida/test/FacturaTest.java
+++ src/test/java/com/distribuida/test/FacturaTest.java
@@ -7,27 +7,54 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FacturaTest {
     private Factura factura;
+    private Cliente cliente;
+
     @BeforeEach
     public void setup(){
-        factura = new Factura(1,"FAC-001",new Date(),15.25,12.67,57.36,1,);
+       factura = new Factura();
+       cliente = new Cliente(1,"1720392870","Juan","Taipe","Av.Guayauil","099986700","pepe@gmail.com");
+       factura = new Factura();
+       factura.setIdFactura(1);
+       factura.setNumFactura("FAC-001");
+       factura.setFecha(new Date());
+       factura.setTotalNeto(1000.00);
+       factura.setIva(15.25);
+       factura.setTotal(1015.35);
+
+       factura.setCliente(cliente);
 
     }
+
     @Test
-    public void testFacturaConstuctorAndGetters(){
-        factura.setIdFactura(1);
-        factura.setNumFactura("FAC-0001");
-        factura.setFecha(new Date());
-        factura.setTotalNeto(100.00);
-        factura.setIva(15.00);
-        factura.setTotal(115.00);
-        //inyeccion de dependencias
-        factura.setCliente(new Cliente());
+    public void testFacturaConstructorAndGetter(){
+        assertAll("Validar datos de factura",
+                () -> assertEquals(1, factura.getIdFactura()),
+                () -> assertEquals("FAC-001", factura.getNumFactura()),
+                () -> assertEquals(1000.00, factura.getTotalNeto()),
+                () -> assertEquals(15.25, factura.getIva()),
+                () -> assertEquals(1015.35, factura.getTotal()),
+                () -> assertEquals("Juan", factura.getCliente().getNombre())
+
+                );
     }
+   @Test
+    public void testFacturaToString(){
+        String str = factura.toString();
+        assertAll("Validar datos de factura",
+                () -> assertTrue(str.contains("1")),
+                () -> assertTrue(str.contains("FAC-001")),
+                () -> assertTrue(str.contains("1000.0")),
+                () -> assertTrue(str.contains("15.2")),
+                () -> assertTrue(str.contains("Juan")),
+                () -> assertTrue(str.contains("pepe@gmail.com"))
+
+                );
+   }
 
 
 
