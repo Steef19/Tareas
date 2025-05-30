cac7083cd93c4ea9aca6f97b6935eef1b4c4e190
1755804091 <bguaman@tecnologicoismac.edu.ec>
2025-05-30 10:29:16 -0500

    prueba3 test A

--- src/test/java/com/distribuida/test/CategoriaTest.java
+++ src/test/java/com/distribuida/test/CategoriaTest.java
@@ -1,4 +1,33 @@
 package com.distribuida.test;
 
+import com.distribuida.entities.Categoria;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CategoriaTest {
+    public Categoria categoria;
+
+    @BeforeEach
+
+    public void setup(){
+        categoria = new Categoria(1,"Aventura","libro de aventuras");
+
+    }
+
+    @Test
+    public void TestCategoriaAndGetter(){
+        assertAll("Validar datos de categoria",
+                () -> assertEquals(1, categoria.getIdCategoria()),
+                () -> assertEquals("Aventura",categoria.getCategoria()),
+                () -> assertEquals("libro de aventuras", categoria.getDescripcion())
+
+                );
+
+    }
+
+
+
 }
